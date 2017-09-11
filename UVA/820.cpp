52f7ed40696cd3d1c6cf3f9166a20780bfe7ca28
Ahmed Al-Ahmed <eng.ahmed2013alahmed@Gmail.com>
2017-09-12 00:19:02 +0300

    Create 820.cpp

---
+++ UVA/820.cpp
@@ -0,0 +1,136 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 11, 2017
+ *      Author: ahmed
+ */
+
+#include <bits/stdc++.h>
+#define lp(i,n) for(int i=0;i<(n);++i)
+#define rep(i,s,n) for(int i=(s);i<=(n);i++)
+#define repr(i,s,n) for(int i=(n);i>=(s);--i)
+#define F first
+#define S second
+#define MP make_pair
+#define pii pair<int,int>
+#define vi vector<int>
+#define vl vector<long>
+#define ll long long int
+#define llu unsigned long long
+#define vll vector<ll>
+#define vllu vector<llu>
+#define clr(m,v) memset(m, v, sizeof m)
+#define pb push_back
+#define clrv(v) v.clear()
+#define all(v) v.begin(), v.end()
+#define si(x) scanf("%d", &x)
+#define si2(x,y) scanf("%d %d", &x, &y)
+#define si3(x,y,z) scanf("%d %d %d", &x, &y, &z)
+#define sll(x) scanf("%lld", &x)
+#define pi(x) printf("%d", x)
+#define pln()   printf("\n")
+#define pll(x)  printf("%lld", x)
+#define cntr(Q) while(Q--)
+#define rfile(f) freopen((f), "r", stdin)
+#define wfile(f) freopen((f), "w", stdout)
+#define INF (int) 1e12
+#define EPS 1e-9
+#define sz(x) x.size()
+using namespace std;
+int N;
+int s,e,m,a,v,c;
+const int MAX = 100 + 10;
+
+
+const int maxnodes = 100 + 10;
+
+int nodes = maxnodes, src, dest;
+int dist[maxnodes], q[maxnodes], work[maxnodes];
+
+struct Edge {
+  int to, rev;
+  int f, cap;
+};
+
+vector<Edge> g[maxnodes];
+
+void addEdge(int s, int t, int cap){
+  Edge a = {t, g[t].size(), 0, cap};
+  Edge b = {s, g[s].size(), 0, cap};
+  g[s].push_back(a);
+  g[t].push_back(b);
+}
+
+bool dinic_bfs() {
+  fill(dist, dist + nodes, -1);
+  dist[src] = 0;
+  int qt = 0;
+  q[qt++] = src;
+  for (int qh = 0; qh < qt; qh++) {
+    int u = q[qh];
+    for (int j = 0; j < (int) g[u].size(); j++) {
+      Edge &e = g[u][j];
+      int v = e.to;
+      if (dist[v] < 0 && e.f < e.cap) {
+        dist[v] = dist[u] + 1;
+        q[qt++] = v;
+      }
+    }
+  }
+  return dist[dest] >= 0;
+}
+
+int dinic_dfs(int u, int f) {
+  if (u == dest)
+    return f;
+  for (int &i = work[u]; i < (int) g[u].size(); i++) {
+    Edge &e = g[u][i];
+    if (e.cap <= e.f) continue;
+    int v = e.to;
+    if (dist[v] == dist[u] + 1) {
+      int df = dinic_dfs(v, min(f, e.cap - e.f));
+      if (df > 0) {
+        e.f += df;
+        g[v][e.rev].f -= df;
+        return df;
+      }
+    }
+  }
+  return 0;
+}
+
+int maxFlow(int _src, int _dest) {
+  src = _src;
+  dest = _dest;
+  int result = 0;
+  while (dinic_bfs()) {
+    fill(work, work + nodes, 0);
+    while (int delta = dinic_dfs(src, INT_MAX))
+      result += delta;
+  }
+  return result;
+}
+
+
+int main() {
+
+//	rfile("in.txt");
+//	wfile("adsas.txt");
+	int cnt = 1;
+	while(cin >> N && N!=0) {
+		for(auto&x : g) x.clear();
+		cin >> s >> e >> m;
+		while(m--) {
+			cin >> a >> v >> c;
+			addEdge(a, v, c);
+		//	addEdge(v, a, c);
+		}
+		cout << "Network " << cnt++ << endl;
+		//cout << "The bandwidth is " << solve() << ".\n";
+		cout << "The bandwidth is " << maxFlow(s,e) << ".\n";
+		cout << endl;
+	}
+
+	return 0;
+}
+
